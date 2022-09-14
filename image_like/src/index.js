0032bcfb5413d0c9f3cf3763e1ad4098ce7f7862
anilikarikatti <anilkarikatti333@gmail.com>
2022-09-14 17:04:43 +0530

    completed

--- image_like/src/index.js
+++ image_like/src/index.js
@@ -10,7 +10,9 @@ const path = require("path");
 
 const bodyParser = require('body-parser');
 
-nunjucks.configure('../views', {
+const connection = require('./connection.js');
+
+nunjucks.configure('views', {
     autoescape: true,
     express: app
 });
@@ -25,7 +27,7 @@ app.use(express.static(static_path));
 // // let d = JSON.stringify(data);
 
 // console.log(typeof(data));
-const port = 3000;
+const port = process.env.PORT || 9001;
 
 // app.set("view engine","html")
 app.get('/',async (req,res)=>{
@@ -46,10 +48,38 @@ app.get('/data',async (req,res)=>{
     // console.log(data);
 });
 
-app.get('/liked',async (req,res)=>{
+app.post('/liked',async (req,res)=>{
    let d = await req.body;
+   let id = d.btn_id;
+   id =Number(id)
+   console.log(typeof(id));
    console.log(d);
+    async function updateLike(){
+        let conn = await connection.main();
+        // console.log(conn);
+        let update_promise = await conn.promise();
+        // let [rows] = await update_promise.query(`select likes from likes where id =${id}`);
+        // let row_like = await rows[0].likes;
+
+        // console.log(typeof(row_like));
+        let [updated_rows] = await update_promise.query(`update likes set likes= likes+1 where id = ${id}`) ;
+        // update likes set likes = 2 where id =1;
+        // res.json(updated_rows);
+
+        // let result = await update_promise.execute(`select likes from likes where id = ${id}`);
+
+        // console.log(result);
+        // res.json(result);
+        // console.log(updated_rows);
+        // res.json({
+        //     "status":200
+        // })
+
+        res.status(200).json();
+    }
+    updateLike();
+
 })
-app.listen(port,()=>{
+app.listen(port,()=>{ 
     console.log(`server listening port ${port}`);
 })
